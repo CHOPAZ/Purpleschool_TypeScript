224067c471fc3bdf8d3119ea7d821c6210315f00
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-04-14 23:58:42 +0500

    Enum

--- app.js
+++ app.js
@@ -1,14 +1,67 @@
 "use strict";
-/* Readonly
+/* Enums
 */
-/* Проблема: несмотря на то что skill константа, мы можем переопределить его элементы */
-const skill = [1, 'Dev'];
-/* Readonly - кортеж, который нельзя изменить */
-const skill2 = [1, 'Dev'];
-const skill3 = ['Dev, DevOps'];
-/* Альтернативная запись для массивов - Джинерик.
-вместо string[] - Array<string>
+/* Числовые enum */
+/*
+Если  навести на SUCCESS, IN_PROCESS, FAILED
+  SUCCESS,  //0
+  IN_PROCESS, // 1
+  FAILED // 2
+
+
+  Можно переопределить их
 */
-const skill4 = ['Dev, DevOps'];
-/* Альтернативная запись для Readonly - Дженерик */
-const skill5 = ['Dev, DevOps'];
+var statusCode;
+(function (statusCode) {
+    statusCode[statusCode["SUCCESS"] = 1] = "SUCCESS";
+    statusCode[statusCode["IN_PROCESS"] = 3] = "IN_PROCESS";
+    statusCode[statusCode["FAILED"] = 10] = "FAILED";
+})(statusCode || (statusCode = {}));
+const res = {
+    message: 'Платеж успешен',
+    statusCode: statusCode.SUCCESS
+};
+/* Строковый enum */
+var statusCode2;
+(function (statusCode2) {
+    statusCode2["SUCCESS"] = "s";
+    statusCode2["IN_PROCESS"] = "p";
+    statusCode2["FAILED"] = "f";
+})(statusCode2 || (statusCode2 = {}));
+const res2 = {
+    message: 'Платеж успешен',
+    statusCode: statusCode2.SUCCESS
+};
+/* Гетерогинные enum - встречается редко*/
+var statusCode3;
+(function (statusCode3) {
+    statusCode3[statusCode3["SUCCESS"] = 1] = "SUCCESS";
+    statusCode3["IN_PROCESS"] = "p";
+    statusCode3["FAILED"] = "f";
+})(statusCode3 || (statusCode3 = {}));
+const res3 = {
+    message: 'Платеж успешен',
+    statusCode: statusCode3.SUCCESS
+};
+/* Но */
+function action(status) {
+}
+action(statusCode3.SUCCESS); //SUCCESS - 1
+action(1); //SUCCESS - 1
+// action('p') // проблема, хотя   IN_PROCESS = 'p', потому что enum числовой
+/* Кейсы enum
+Например:
+1. справочник кодов ответов,
+2. справочник величин,
+3. справочник направления движения (левоЮ, право, вверх, вниз).
+4. роли и т.д.
+*/
+/* enum могут быть расчетные */
+function compute() {
+    return 3;
+}
+var Roles;
+(function (Roles) {
+    Roles[Roles["ADMIN"] = 2] = "ADMIN";
+    Roles[Roles["USER"] = compute()] = "USER";
+})(Roles || (Roles = {}));
--- app.ts
+++ app.ts
@@ -1,19 +1,81 @@
-/* Readonly
+/* Enums
 */
 
 
-/* Проблема: несмотря на то что skill константа, мы можем переопределить его элементы */
-const skill: [number, string] = [1, 'Dev'];
+/* Числовые enum */
+/* 
+Если  навести на SUCCESS, IN_PROCESS, FAILED
+  SUCCESS,  //0
+  IN_PROCESS, // 1
+  FAILED // 2
 
-/* Readonly - кортеж, который нельзя изменить */
-const skill2: readonly[number, string] = [1, 'Dev'];
 
-const skill3: readonly string[] = ['Dev, DevOps'];
-
-/* Альтернативная запись для массивов - Джинерик.
-вместо string[] - Array<string>
+  Можно переопределить их
 */
-const skill4: Array<string> = ['Dev, DevOps'];
+enum statusCode {
+  SUCCESS = 1,
+  IN_PROCESS = 3,
+  FAILED = 10
+}
+const res = { 
+  message: 'Платеж успешен',
+  statusCode: statusCode.SUCCESS
+}
 
-/* Альтернативная запись для Readonly - Дженерик */
-const skill5: ReadonlyArray<string> = ['Dev, DevOps'];
+/* Строковый enum */
+enum statusCode2 {
+  SUCCESS = 's',
+  IN_PROCESS = 'p',
+  FAILED = 'f'
+}
+const res2 = { 
+  message: 'Платеж успешен',
+  statusCode: statusCode2.SUCCESS
+}
+
+/* Гетерогинные enum - встречается редко*/
+enum statusCode3 {
+  SUCCESS = 1,
+  IN_PROCESS = 'p',
+  FAILED = 'f'
+}
+const res3 = { 
+  message: 'Платеж успешен',
+  statusCode: statusCode3.SUCCESS
+}
+
+
+/* Но */
+
+function action(status: statusCode3) {
+
+}
+action(statusCode3.SUCCESS) //SUCCESS - 1
+action(1) //SUCCESS - 1
+// action('p') // проблема, хотя   IN_PROCESS = 'p', потому что enum числовой
+
+/* Кейсы enum
+Например:
+1. справочник кодов ответов,
+2. справочник величин,
+3. справочник направления движения (левоЮ, право, вверх, вниз).
+4. роли и т.д.
+*/
+
+
+/* enum могут быть расчетные */
+
+function compute() {
+  return 3
+}
+enum Roles {
+  ADMIN = 1 * 2,
+  USER = compute()
+}
+
+const enum Roles2 {
+  ADMIN = 1,
+  USER = 2
+}
+
+const res4 = Roles2.ADMIN
