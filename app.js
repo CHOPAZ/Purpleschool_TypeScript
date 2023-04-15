e0a38dc9345dc0b77c8458eaaedef11bf50750f1
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-04-15 23:03:57 +0500

    Practice - Types function

--- app.js
+++ app.js
@@ -1,67 +1,28 @@
 "use strict";
-/* Enums
+/* Упражнение - Типизируем функицию
 */
-/* Числовые enum */
-/*
-Если  навести на SUCCESS, IN_PROCESS, FAILED
-  SUCCESS,  //0
-  IN_PROCESS, // 1
-  FAILED // 2
-
-
-  Можно переопределить их
-*/
-var statusCode;
-(function (statusCode) {
-    statusCode[statusCode["SUCCESS"] = 1] = "SUCCESS";
-    statusCode[statusCode["IN_PROCESS"] = 3] = "IN_PROCESS";
-    statusCode[statusCode["FAILED"] = 10] = "FAILED";
-})(statusCode || (statusCode = {}));
-const res = {
-    message: 'Платеж успешен',
-    statusCode: statusCode.SUCCESS
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
 };
-/* Строковый enum */
-var statusCode2;
-(function (statusCode2) {
-    statusCode2["SUCCESS"] = "s";
-    statusCode2["IN_PROCESS"] = "p";
-    statusCode2["FAILED"] = "f";
-})(statusCode2 || (statusCode2 = {}));
-const res2 = {
-    message: 'Платеж успешен',
-    statusCode: statusCode2.SUCCESS
-};
-/* Гетерогинные enum - встречается редко*/
-var statusCode3;
-(function (statusCode3) {
-    statusCode3[statusCode3["SUCCESS"] = 1] = "SUCCESS";
-    statusCode3["IN_PROCESS"] = "p";
-    statusCode3["FAILED"] = "f";
-})(statusCode3 || (statusCode3 = {}));
-const res3 = {
-    message: 'Платеж успешен',
-    statusCode: statusCode3.SUCCESS
-};
-/* Но */
-function action(status) {
+var QuestionStatus;
+(function (QuestionStatus) {
+    QuestionStatus["PUBLISHED"] = "published";
+    QuestionStatus["DRAFT"] = "draft";
+    QuestionStatus["DELETED"] = "deleted";
+})(QuestionStatus || (QuestionStatus = {}));
+function getFaqs(req) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const res = yield fetch('/faqs', {
+            method: 'POST',
+            body: JSON.stringify(req)
+        });
+        const data = yield res.json();
+        return data;
+    });
 }
-action(statusCode3.SUCCESS); //SUCCESS - 1
-action(1); //SUCCESS - 1
-// action('p') // проблема, хотя   IN_PROCESS = 'p', потому что enum числовой
-/* Кейсы enum
-Например:
-1. справочник кодов ответов,
-2. справочник величин,
-3. справочник направления движения (левоЮ, право, вверх, вниз).
-4. роли и т.д.
-*/
-/* enum могут быть расчетные */
-function compute() {
-    return 3;
-}
-var Roles;
-(function (Roles) {
-    Roles[Roles["ADMIN"] = 2] = "ADMIN";
-    Roles[Roles["USER"] = compute()] = "USER";
-})(Roles || (Roles = {}));
--- app.ts
+++ app.ts
@@ -1,81 +1,25 @@
-/* Enums
+/* Упражнение - Типизируем функицию
 */
 
-
-/* Числовые enum */
-/* 
-Если  навести на SUCCESS, IN_PROCESS, FAILED
-  SUCCESS,  //0
-  IN_PROCESS, // 1
-  FAILED // 2
-
-
-  Можно переопределить их
-*/
-enum statusCode {
-  SUCCESS = 1,
-  IN_PROCESS = 3,
-  FAILED = 10
+enum QuestionStatus {
+  PUBLISHED = 'published',
+  DRAFT = 'draft',
+  DELETED = 'deleted'
 }
-const res = { 
-  message: 'Платеж успешен',
-  statusCode: statusCode.SUCCESS
+async function getFaqs(req: {
+  topicId: number;
+  status?: QuestionStatus
+}): Promise<{
+  question: string;
+  answer: string;
+  tags: string[];
+  likes: number;
+  status: string
+}[]> {
+	const res = await fetch('/faqs', {
+		method: 'POST',
+		body: JSON.stringify(req)
+	});
+	const data = await res.json();
+	return data;
 }
-
-/* Строковый enum */
-enum statusCode2 {
-  SUCCESS = 's',
-  IN_PROCESS = 'p',
-  FAILED = 'f'
-}
-const res2 = { 
-  message: 'Платеж успешен',
-  statusCode: statusCode2.SUCCESS
-}
-
-/* Гетерогинные enum - встречается редко*/
-enum statusCode3 {
-  SUCCESS = 1,
-  IN_PROCESS = 'p',
-  FAILED = 'f'
-}
-const res3 = { 
-  message: 'Платеж успешен',
-  statusCode: statusCode3.SUCCESS
-}
-
-
-/* Но */
-
-function action(status: statusCode3) {
-
-}
-action(statusCode3.SUCCESS) //SUCCESS - 1
-action(1) //SUCCESS - 1
-// action('p') // проблема, хотя   IN_PROCESS = 'p', потому что enum числовой
-
-/* Кейсы enum
-Например:
-1. справочник кодов ответов,
-2. справочник величин,
-3. справочник направления движения (левоЮ, право, вверх, вниз).
-4. роли и т.д.
-*/
-
-
-/* enum могут быть расчетные */
-
-function compute() {
-  return 3
-}
-enum Roles {
-  ADMIN = 1 * 2,
-  USER = compute()
-}
-
-const enum Roles2 {
-  ADMIN = 1,
-  USER = 2
-}
-
-const res4 = Roles2.ADMIN
