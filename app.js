c973af35e7bded0408bd1fb39f5643c463936dbc
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-04-17 22:13:22 +0500

    Oprional

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 "use strict";
-/* Types или Interfaces */
+/* Optional */
 const user = {
-    name: 'Pavel',
-    age: 25
+    login: 'asd@asd.ru',
 };
-/* Рекомендации
-1. type использовать для примитивных типов
-2. interface работа с объектами, классами
-
-*/ 
+/* Использование опциональный тип в функциях
+  в функицях number | undefined - эквивалентно ?
+*/
+function multiply(first, second) {
+    if (!second) {
+        return first * first;
+    }
+    return first * second;
+}
+function testPass(user) {
+    var _a;
+    const type = (_a = user.password) === null || _a === void 0 ? void 0 : _a.type;
+    const type2 = user.password ? user.password.type : undefined;
+    const type3 = user.password.type; // - 100% пароль будет не undefined
+}
+function test(param) {
+    const t = param !== null && param !== void 0 ? param : multiply(5);
+}
--- app.ts
+++ app.ts
@@ -1,34 +1,49 @@
-/* Types или Interfaces */
+/* Optional */
 
 
-
-/* Преимущества Interface
-1. один интерфейс дополняет другой интерфейс (код ниже)
-
-Минус в этом, если это наш интерфейс, и получится сложность в ревью кода
-Полезно, когда используется какая то библиотека, и необходимо ее доопределить
-*/
+/* Если какой то параметр является необязательный то исп. ? */
 interface User {
-  name: string
+  login: string;
+  password?: string;
+  // password: string | undefined - не эквивалентно ?
 }
 
-interface User {
-  age: number
+type User2 = {
+  login: string;
+  password?: string;
 }
 
-const user: User ={
-  name: 'Pavel',
-  age: 25
+const user: User = {
+  login: 'asd@asd.ru',
 }
 
 
-/* Преимущества type */
-
-type ID = string | number // union
-
-
-/* Рекомендации
-1. type использовать для примитивных типов
-2. interface работа с объектами, классами
-
+/* Использование опциональный тип в функциях
+  в функицях number | undefined - эквивалентно ?
 */
+
+function multiply(first: number, second?: number): number {
+  if (!second) {
+    return first * first
+  }
+  return first * second
+}
+
+/* Использование опциональный тип в объекте */
+interface UserPro {
+  login: string;
+  password?: {
+    type: 'primary' | 'secondary'
+  };
+}
+
+function testPass(user: UserPro) {
+  const type = user.password?.type
+  const type2 = user.password ? user.password.type : undefined
+  const type3 = user.password!.type // - 100% пароль будет не undefined
+}
+
+function test(param?: string) {
+  const t = param ?? multiply(5)
+}
+
