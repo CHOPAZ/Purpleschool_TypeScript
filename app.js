860c9b962e2b9ba5873c4b37c870d8111181c785
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-04-18 23:04:50 +0500

    Void

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 "use strict";
-/* Упражнение - типизируем ответ сервера */
-/* Ответ */
-var PaymentStatus;
-(function (PaymentStatus) {
-    PaymentStatus["SUCCESS"] = "success";
-    PaymentStatus["FAILED"] = "failed";
-})(PaymentStatus || (PaymentStatus = {}));
-function get() {
+/* void - обозначает что эта функция ничего не возвращаетcz */
+function logId(id) {
+    console.log(id);
 }
+const a = logId(1); // тип a - void
+/*
+  Если функция из любой ветки хоть что-то вернула будет number | undefined
+  Если функция из любой ветки ничего не возвращает будет void
+*/
+function multiply(f, s) {
+    if (!s) {
+        return f * f;
+    }
+}
+const f1 = () => { };
+/* Работает, потмоу что возврат будет игнорироваться. Практическое применение, когда хотим сделать например перебор массива */
+const f2 = () => {
+    return true;
+};
+const b = f2(); //void
+const skills = ['Dev', 'DevOps'];
+const user = {
+    skills: ['']
+};
+skills.forEach((e) => user.skills.push(e));
+/* Благодаря void, мы получаем совместимость использования forEach, который ожидает в результате void и push, в результате который ожидает какое-то число
+
+  Ключевое различие между void и undefined, если функция ничего не возвращает она может быть void или undefined, но когда мы типизируем функцию и хотим игнорировать возврат мы типищзируем как возврат void
+*/ 
--- app.ts
+++ app.ts
@@ -1,44 +1,43 @@
-/* Упражнение - типизируем ответ сервера */
+/* void - обозначает что эта функция ничего не возвращаетcz */
 
-
-/* Первый вариант нативный */
-/* Интерфейс платежа */
-interface Payment {
-  sum: number;
-  from: number;
-  to: number;
+function logId(id: string | number): void {
+  console.log(id);
 }
 
-/* Запрос */
-interface PaymentRequest extends Payment{}
+const a = logId(1) // тип a - void
 
-/* Ответ */
-enum PaymentStatus {
-  SUCCESS = 'success',
-  FAILED = 'failed'
+
+/* 
+  Если функция из любой ветки хоть что-то вернула будет number | undefined
+  Если функция из любой ветки ничего не возвращает будет void
+*/
+function multiply(f: number, s?: number) { //тип функции которая возвращает number | undefined
+  if(!s) {
+    return f * f
+  }
 }
 
-interface DataSuccess extends Payment {
-  databaseId: number;
+/* Кейсы импользования void */
+
+type voidFunc = () => void;
+
+const f1: voidFunc = () => {}
+
+/* Работает, потмоу что возврат будет игнорироваться. Практическое применение, когда хотим сделать например перебор массива */
+const f2: voidFunc = () => {
+  return true
+}
+const b = f2(); //void
+
+
+const skills = ['Dev', 'DevOps'];
+const user = {
+  skills: ['']
 }
 
-interface DataFailed {
-  errorMessage: string;
-  errorCode: number
-}
+skills.forEach((e) => user.skills.push(e))
 
-interface ResponceSuccess {
-  status: PaymentStatus.SUCCESS
-  data: DataSuccess
-}
+/* Благодаря void, мы получаем совместимость использования forEach, который ожидает в результате void и push, в результате который ожидает какое-то число
 
-interface ResponceFailed {
-  status: PaymentStatus.FAILED
-  data: DataFailed
-}
-
-function get(): ResponceSuccess | ResponceFailed {
-
-}
-
-
+  Ключевое различие между void и undefined, если функция ничего не возвращает она может быть void или undefined, но когда мы типизируем функцию и хотим игнорировать возврат мы типищзируем как возврат void
+*/
