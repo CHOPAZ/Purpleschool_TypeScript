82fbb5a12df0fcf6474b7ebb45e602e722f405d4
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-04-26 20:15:00 +0500

    Работа с this

--- app.js
+++ app.js
@@ -1,27 +1,60 @@
 "use strict";
-/* Статические свойства
-  Статичные классы не могут быть, из-за того что JS не ограничен только классами. Поэтому можно статичным задать не класс, а какие-то свойства или методы класса
+/*  Работа с this
 */
-class UserService {
-    constructor(id) {
+class Payment {
+    constructor() {
+        this.date = new Date();
     }
-    static getDB(id) {
-        // return this.db.findById(id);
-        /* Правильная запись */
-        return UserService.db.findById(id);
-    }
-    create() {
-        UserService.db;
+    getDate() {
+        return this.date;
     }
 }
-/* некий инициализатор статичного класса. Асинхронные действия нельзя делать */
-(() => {
-    /* пример */
-    UserService.db = 'afsa';
-})();
-/* Для того что бы обратится к свойству db, не требуется создавать инстанс */
-UserService.db;
-UserService.getDB(2);
-// UserService.create() - не сможем получить
-const inst = new UserService(1);
-inst.create(); // можем получить доступ
+const p = new Payment();
+const user = {
+    id: 1,
+    paymentDate: p.getDate.bind(p)
+};
+console.log(p.getDate());
+console.log(user.paymentDate());
+/* Как указать ts что  в данном случае будет ошибка, т.е если не забаиндить this, то будет undefinde */
+class Payment2 {
+    constructor() {
+        this.date = new Date();
+    }
+    getDate() {
+        return this.date;
+    }
+}
+const p2 = new Payment2();
+const user2 = {
+    id: 1,
+    paymentDate: p2.getDate.bind(p2)
+};
+console.log(p2.getDate());
+console.log(user2.paymentDate());
+/* Одна из возможностей не терять контекст использовать стрелочные функции */
+class Payment3 {
+    constructor() {
+        this.date = new Date();
+        this.getDateArrow = () => {
+            return this.date;
+        };
+    }
+    getDate() {
+        return this.date;
+    }
+}
+const p3 = new Payment3();
+const user3 = {
+    id: 1,
+    paymentDate: p3.getDate.bind(p3),
+    paymentDateArrow: p3.getDateArrow
+};
+console.log(user3.paymentDateArrow());
+/* Случаи когда стрелочная функция ен будет работать */
+class PaymentPersistent extends Payment3 {
+    save() {
+        return super.getDateArrow();
+    }
+}
+console.log(new PaymentPersistent().save()); // .getDateArrow is not a function - потому что в прототипе Payment не будет функции стрелочной, но если заенить на  return this.getDateArrow() - работать будет
--- app.ts
+++ app.ts
@@ -1,37 +1,73 @@
-/* Статические свойства
-  Статичные классы не могут быть, из-за того что JS не ограничен только классами. Поэтому можно статичным задать не класс, а какие-то свойства или методы класса
+/*  Работа с this
 */
 
-class UserService {
-  static db: any;
-  private static db2: any;
 
-  constructor(id: number) {
+class Payment {
+  private date: Date = new Date();
 
-  }
-
-  static getDB(id: number) {
-    // return this.db.findById(id);
-    /* Правильная запись */
-    return UserService.db.findById(id);
-  }
-
-  create() {
-    UserService.db
-  }
-
-  /* некий инициализатор статичного класса. Асинхронные действия нельзя делать */
-  static {
-    /* пример */
-    UserService.db = 'afsa'
+  getDate() {
+    return this.date
   }
 }
 
-/* Для того что бы обратится к свойству db, не требуется создавать инстанс */
-UserService.db
-UserService.getDB(2)
-// UserService.create() - не сможем получить
-const inst = new UserService(1);
-inst.create() // можем получить доступ
+const p = new Payment()
 
+const user = {
+  id: 1,
+  paymentDate: p.getDate.bind(p)
+}
 
+console.log(p.getDate());
+console.log(user.paymentDate());
+
+/* Как указать ts что  в данном случае будет ошибка, т.е если не забаиндить this, то будет undefinde */
+class Payment2 {
+  private date: Date = new Date();
+
+  getDate(this: Payment2) { //теперь, ts подсветит ошибку, если не сделать bind
+    return this.date
+  }
+}
+
+const p2 = new Payment2()
+
+const user2 = {
+  id: 1,
+  paymentDate: p2.getDate.bind(p2)
+}
+
+console.log(p2.getDate());
+console.log(user2.paymentDate());
+
+/* Одна из возможностей не терять контекст использовать стрелочные функции */
+class Payment3 {
+  private date: Date = new Date();
+
+  getDate(this: Payment3) { 
+    return this.date
+  }
+
+  getDateArrow = () => {
+    return this.date
+  }
+}
+
+const p3 = new Payment3();
+
+const user3 = {
+  id: 1,
+  paymentDate: p3.getDate.bind(p3),
+  paymentDateArrow: p3.getDateArrow
+}
+
+console.log(user3.paymentDateArrow());
+
+/* Случаи когда стрелочная функция ен будет работать */
+
+class PaymentPersistent extends Payment3 {
+  save() {
+    return super.getDateArrow()
+  }
+}
+
+console.log(new PaymentPersistent().save());// .getDateArrow is not a function - потому что в прототипе Payment не будет функции стрелочной, но если заенить на  return this.getDateArrow() - работать будет
