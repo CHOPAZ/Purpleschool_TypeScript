f45cb0a9301c2acfcda55df20c8fcbbe693cccd0
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-05-03 21:01:28 +0500

    Mixins

--- app.js
+++ app.js
@@ -1,19 +1,63 @@
 "use strict";
 /*
-  Generic классы
+  Mixins - реализуют возможность наследования не только от одного класса, но и от нескольких классов. Либо mixin используются как добавление примисей дополнительных свойст тому или иному объекту без явного наследования
+
+
+  Кейсы:
+  DSI модель, которую можно использовать в рамках тестировани
+  Доп наследование от  нескольких элементов, которые мы не можем реализовать в обычном ts
 */
-class Resp {
-    constructor(data, error) {
-        this.data = data;
-        this.error = error;
+class List {
+    constructor(items) {
+        this.items = items;
     }
 }
-const res = new Resp('data', 0);
-/* Наследование */
-class HTTPResp extends Resp {
-    setCode(code) {
-        this.code = code;
+/* Mixin - функция,  */
+/* Расширим функциона List, добавив ему метод first */
+/* простым способом - наследование */
+class ExtendsListClass extends List {
+    first() {
+        return this.items[0];
     }
 }
-const res2 = new HTTPResp();
-/* Generic полезны в классах, когда есть некоторый универсальный верхняя часть  объект например Resp<D, E>, и венутри есть переменные поля, которые могут зависить от реалдизации, например data которая может быть любым типом*/
+/* через mixin */
+function ExtendsListMixin(Base) {
+    return class ExtendsListMixin extends Base {
+        first() {
+            return this.items[0];
+        }
+    };
+}
+/* Описание кода выше:
+  1. в функцию ExtendsListMixin должны передать какой то класс ( не инстанс класса) - Base: TBase, этот клдасс должен наследовать ListType
+  2. возвращаем класс, который наследует переданный класс
+
+*/
+const list = ExtendsListMixin(List); // в константе получили сам класс, не его инстанс
+const res = new list(['first', 'second']);
+console.log(res.first());
+class List2 {
+    constructor(items) {
+        this.items = items;
+    }
+}
+class Accordion2 {
+}
+function ExtendsListMixin2(Base) {
+    return class ExtendsListMixin2 extends Base {
+        first() {
+            return this.items[0];
+        }
+    };
+}
+class AccordionList2 {
+    constructor(items) {
+        this.items = items;
+    }
+}
+const list2 = ExtendsListMixin(AccordionList2);
+const res2 = new list2(['first', 'second']);
+console.log(res2.first());
+console.log(res2.isOpend);
+console.log(res2.items);
+/* Мы получили extends фактически от двух классов */
--- app.ts
+++ app.ts
@@ -1,31 +1,92 @@
 /*  
-  Generic классы
+  Mixins - реализуют возможность наследования не только от одного класса, но и от нескольких классов. Либо mixin используются как добавление примисей дополнительных свойст тому или иному объекту без явного наследования
+
+
+  Кейсы:
+  DSI модель, которую можно использовать в рамках тестировани
+  Доп наследование от  нескольких элементов, которые мы не можем реализовать в обычном ts
 */
 
-class Resp<D, E> {
-  data?: D;
-  error?: E;
+/* Тип constructor */
+type Constructor = new (...args: any[]) => {}
 
-  constructor(data?: D, error?: E) {
-    this.data = data;
-    this.error = error
+/* Уличшить конструктор, сузив его использование при помощи Generic
+  T = {} - значение по умолчанию, т.е если не передадим Т, то он по умолчанию является чем угодно
+*/
+type GConstructor<T = {}> = new (...args: any[]) => T
+
+class List {
+  constructor(public items: string[]) {}
+}
+
+type ListType = GConstructor<List>
+
+/* Mixin - функция,  */
+
+/* Расширим функциона List, добавив ему метод first */
+/* простым способом - наследование */
+class ExtendsListClass extends List {
+  first() {
+    return this.items[0]
   }
 }
 
-
-const res = new Resp<string, number>('data', 0)
-
-
-/* Наследование */
-
-class HTTPResp<F> extends Resp<string, number>{
-  code: F;
-
-  setCode(code: F) {
-    this.code = code
+/* через mixin */
+function ExtendsListMixin<TBase extends ListType>(Base: TBase) {
+  return class ExtendsListMixin extends Base {
+    first() {
+      return this.items[0]
+    }
   }
 }
 
-const res2 = new HTTPResp();
+/* Описание кода выше:
+  1. в функцию ExtendsListMixin должны передать какой то класс ( не инстанс класса) - Base: TBase, этот клдасс должен наследовать ListType
+  2. возвращаем класс, который наследует переданный класс
 
-/* Generic полезны в классах, когда есть некоторый универсальный верхняя часть  объект например Resp<D, E>, и венутри есть переменные поля, которые могут зависить от реалдизации, например data которая может быть любым типом*/
+*/
+
+const list = ExtendsListMixin(List) // в константе получили сам класс, не его инстанс
+const res = new list(['first', 'second']);
+console.log(res.first());
+
+
+/* Когда использовать mixin и в чем его преимущетсва
+  1. Позволяет писать еще вариант наследования, когда моджно спокойно примиксовать еще одно свойство к исходному класса
+  2. Позволяет делать наследование нескольких классов одновременно
+*/
+
+type GConstructor2<T = {}> = new (...args: any[]) => T
+
+class List2 {
+  constructor(public items: string[]) {}
+}
+class Accordion2 {
+  isOpend: boolean;
+
+}
+
+type ListType2 = GConstructor2<List2>
+type AccordionType2 = GConstructor2<Accordion2>
+
+function ExtendsListMixin2<TBase extends ListType2 & AccordionType2>(Base: TBase) {
+  return class ExtendsListMixin2 extends Base {
+    first() {
+      return this.items[0]
+    }
+  }
+}
+
+class AccordionList2 {
+  isOpend: boolean;
+  constructor(public items: string[]) {}
+}
+
+
+const list2 = ExtendsListMixin(AccordionList2)
+const res2 = new list2(['first', 'second']);
+console.log(res2.first());
+console.log(res2.isOpend);
+console.log(res2.items);
+
+/* Мы получили extends фактически от двух классов */
