4b1be818db07a6f60667aead0f4efccac316b899
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-05-14 21:17:08 +0500

    Practice - validate form

--- app.ts
+++ app.ts
@@ -1,52 +1,33 @@
 /*  
-  Mapped types
-*/
-type MOdifier = 'read' | 'update' | 'create';
+  Упражненеие - валидация форм.
 
-type UserRoles = {
-  customres?: MOdifier;
-  projects?: MOdifier;
-  adminPanal?: MOdifier;
-}
-
-/* стандартная запись, но в случае изменения UserRoles нужно менять и UserAccess1  */
-type UserAccess1 = {
-  customres?: boolean;
-  projects?: boolean;
-  adminPanal?: boolean;
-}
-
-/*Mapped types  */
-type ModifierToAccess<Type> ={ 
-  [Property in keyof Type]: boolean // берем каждый ключ в UserRoles и оно должно соответсвовать boolean
-}
-
-type UserAccess2 = ModifierToAccess<UserRoles>
-
-/*  */
-type ModifierToAccess2<Type> ={ 
-  [Property in keyof Type]-?: boolean // -? все свойства обязательны, +? - все свойства необязательны
-}
-type UserAccess3 = ModifierToAccess2<UserRoles>
-
-/*  */
-type ModifierToAccess3<Type> ={ 
-  +readonly [Property in keyof Type]-?: boolean //+readonly - добавляем readonly ко всем типам
-}
-
-/* Переименовывание свойст */
-type ModifierToAccess4<Type> ={ 
-  +readonly [Property in keyof Type as `canAccess${string & Property}`]-?: boolean //+readonly - добавляем readonly ко всем типам
-}
-
-type UserAccess4 = ModifierToAccess4<UserRoles> /*  
-  type UserAccess4 = {
-    readonly canAccesscustomres: boolean;
-    readonly canAccessprojects: boolean;
-    readonly canAccessadminPanal: boolean;
-  } 
+  Необходимо сделать тип для результата валидации
+  формы, основываясь на типе формы
 */
 
-/* Mapped types - полезно использовать когда есть совпадающие ключи, но в объекте нужны другие значения
-  Дополнительно полезен, когде необходимо сделать какие то опциональные вещи
-*/
+interface IForm {
+  name: string;
+  password: string;
+}
+
+const form: IForm = {
+  name: 'Vasya',
+  password: '123'
+}
+
+
+/* результат валидации */
+const formValidation: Validation<IForm> = {
+  name: {
+    isValid: true
+  },
+  password: {
+    isValid: false,
+    erroeMessage: 'Должен содержвать длиннее 5 символов'
+  }
+}
+
+type Validation<T> = {
+  [key in keyof T]: {isValid: true} | {isValid: false, erroeMessage: string}
+}
+
