48ec373219e9d8bc05996e156fb6a19e43dafe24
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-05-15 21:21:09 +0500

    Partial, Required Readonly

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 "use strict";
 /*
-  Template Literal Types
+  Служебные типы
+
+  Partial,
+  Required, -   все эти типы содержат под капотом Mapped Types, если провалиться внутрь описания то увидем следующееЭ
+  Readonly
+
+  Partial -  type Partial<T> = {
+    [P in keyof T]?: T[P];
+  };
+
+  Required - type Required<T> = {
+    [P in keyof T]-?: T[P];
+  };
+
+  Readonly - type Readonly<T> = {
+    readonly [P in keyof T]: T[P];
+  };
 */
-const a = {
-    result: 'httpError'
-};
+const p = {};
--- app.ts
+++ app.ts
@@ -1,28 +1,61 @@
 /*  
-  Template Literal Types
+  Служебные типы
+
+  Partial,
+  Required, -   все эти типы содержат под капотом Mapped Types, если провалиться внутрь описания то увидем следующееЭ
+  Readonly
+
+  Partial -  type Partial<T> = {
+    [P in keyof T]?: T[P];
+  };
+
+  Required - type Required<T> = {
+    [P in keyof T]-?: T[P];
+  };
+
+  Readonly - type Readonly<T> = {
+    readonly [P in keyof T]: T[P];
+  };
 */
 
-type ReadOrWrite = 'read' | 'write';
-type Access = `can${ReadOrWrite}`; // type Access = "canread" | "canwrite"
-type Access2 = `can${Uppercase<ReadOrWrite>}`; // type Access2 = "canREAD" | "canWRITE"
-type Access3 = `can${Capitalize<ReadOrWrite>}`; // type Access3 = "canRead" | "canWrite"
-
-type Bulk = 'bulk' | '';
-type Access4 = `can${Capitalize<ReadOrWrite>}${Capitalize<Bulk>}`; //type Access4 = "canRead" | "canWrite" | "canReadBulk" | "canWriteBulk"
-
-
-/*  */
-type ErrorOrSuccess = 'error' | 'success';
-interface ResponceT {
-  result: `http${Capitalize<ErrorOrSuccess>}`
+interface User {
+  name: string;
+  age?: number;
+  email: string;
 }
+/* Partial - используется не так часто, необходимость его следетелствует о необходимости  отдельного типа, либо нет необходимости что то типизировать и выкинуть часть савойст */
+/* использовать User как interface, но сделать поля не обязательными */
+type partial = Partial<User>; /* 
+  type p = {
+    name?: string | undefined;
+    age?: number | undefined;
+    email?: string | undefined;
+}
+*/
+const p: partial = {}
 
-const a: ResponceT = {
-  result: 'httpError'
-};
 
+/* Required - делает поля обязательными */
+type required = Required<User>; /* 
+  type required = {
+    name: string;
+    age: number; - age стал обязательным
+    email: string;
+}
+*/
 
-/* Обратно распокавать составной тип */
-
-type ReadOrWriteBulk<T> = T extends `can${infer R}` ? R : never;
-type T = ReadOrWriteBulk<Access4>; //type T = "Read" | "Write" | "ReadBulk" | "WriteBulk"
+/* Readonly -  */
+type readonly = Readonly<User>; /* 
+  type readonly = {
+    readonly name: string;
+    readonly age?: number | undefined;
+    readonly email: string;
+}
+*/
+type requiredAndReadonly = Required<Readonly<User>>; /* 
+  type requiredAndReadonly = {
+    readonly name: string;
+    readonly age: number;
+    readonly email: string;
+}
+*/
