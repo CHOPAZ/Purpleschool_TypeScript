0634f19b32d4058ba22e368cb926e359d78c5885
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-05-18 21:58:10 +0500

    Фабрика декораторов

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 "use strict";
 /*
-  Декоратор класса
+  Фабрика декораторов
 */
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
@@ -8,6 +8,8 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+/* Порядок вызова декораторов */
+// @nullUser
 let UserService = class UserService {
     constructor() {
         this.users = 1000;
@@ -17,12 +19,30 @@ let UserService = class UserService {
     }
 };
 UserService = __decorate([
-    nullUser,
-    threeUserAdvanced
+    setUser(10)
+    // @threeUserAdvanced
+    // @setUserAdvanced(4)
+    ,
+    log()
 ], UserService);
 function nullUser(target) {
     target.prototype.users = 0;
 }
+/* Необходимо вместо 0 установить определенное число пользователей */
+function setUser(users) {
+    console.log('setUsers init');
+    return (target) => {
+        console.log('setUsers run');
+        target.prototype.users = users;
+    };
+}
+function log() {
+    console.log('log init');
+    return (target) => {
+        console.log('log run');
+    };
+}
+/* вызов SetUser как декоратор ^ */
 console.log(new UserService().getUsersInDatabase()); //1000
 /* ^ Получили 1000, а хотели 0
   Потому что function nullUser выполняется еще на этапе компиляции приложения, а потом класс UserService
@@ -37,6 +57,17 @@ function threeUserAdvanced(constructors) {
             super(...arguments);
             this.users = 3;
         }
+    };
+}
+/* необходимо вместо 3 установить определенное число пользователей */
+function setUserAdvanced(users) {
+    return (constructors) => {
+        return class extends constructors {
+            constructor() {
+                super(...arguments);
+                this.users = users;
+            }
+        };
     };
 }
 /* threeUserAdvanced - некоторый конструированный класс, не функция, и от этого класса будем наследоваться анонимным классом, где применяем поведение */
--- app.ts
+++ app.ts
@@ -1,5 +1,5 @@
 /*  
-  Декоратор класса
+  Фабрика декораторов
 */
 
 /* Пример */
@@ -8,8 +8,12 @@ interface IUserService {
   getUsersInDatabase(): number;
 }
 
-@nullUser
-@threeUserAdvanced
+/* Порядок вызова декораторов */
+// @nullUser
+@setUser(10)
+// @threeUserAdvanced
+// @setUserAdvanced(4)
+@log()
 class UserService implements IUserService {
   users: number = 1000;
   getUsersInDatabase(): number {
@@ -22,8 +26,25 @@ function nullUser(target: Function) {
   target.prototype.users = 0;
 }
 
-console.log(new UserService().getUsersInDatabase()); //1000
+/* Необходимо вместо 0 установить определенное число пользователей */
+function setUser(users: number) {
+  console.log('setUsers init');
+  return (target: Function) => {
+    console.log('setUsers run');
+    target.prototype.users = users;
+  }
+}
 
+function log() {
+  console.log('log init');
+  return (target: Function) => {
+    console.log('log run');
+  }
+}
+
+/* вызов SetUser как декоратор ^ */
+
+console.log(new UserService().getUsersInDatabase()); //1000
 /* ^ Получили 1000, а хотели 0
   Потому что function nullUser выполняется еще на этапе компиляции приложения, а потом класс UserService
   Если у класса уббрать присвоение 1000, т.е users: number; то тогда бы получили 0
@@ -39,4 +60,12 @@ function threeUserAdvanced<T extends {new(...args: any[]): {}}>(constructors: T)
   }
 }
 
+/* необходимо вместо 3 установить определенное число пользователей */
+function setUserAdvanced(users: number) {
+  return <T extends {new(...args: any[]): {}}>(constructors: T) => {
+    return class extends constructors {
+      users = users;
+    }
+  }
+}
 /* threeUserAdvanced - некоторый конструированный класс, не функция, и от этого класса будем наследоваться анонимным классом, где применяем поведение */
