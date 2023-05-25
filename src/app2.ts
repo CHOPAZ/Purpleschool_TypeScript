8e76efda26d2b37e31fbfad266a580dbca1bdaf5
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-05-25 22:46:49 +0500

    Import and export

--- src/app2.ts
+++ src/app2.ts
@@ -1,5 +1,15 @@
-import {A} from './module/app.js'
 import {a} from './module/app.js'
-console.log(A.a);
+import run from './module/app.js'; // вместо run можно назвать как угодно. Это импорт только  default
+import * as all from './module/app.js' // импорт всего, и default тоже
+import {Test as CL} from './module/app.js'; // переименнование класса Test в CL
+// import { MyType } from './module/app.js'; // импорт типа
+
+/* специализированный синтаксис только для ts */
+// import type { MyType } from './module/app.js';
+import {type  MyType } from './module/app.js'; //с версии ts 4...
+
+
+new CL()
+run()
 console.log(a);
 
--- src/module/app.ts
+++ src/module/app.ts
@@ -1,26 +1,14 @@
 /*  
   Модульность и библиотеки
 
-  Модульность на frontend
-  Существует две оцпии:
-  1. Собираем все в один bundle,  при помощи webpack, rollup
-  2. Если использовать современные модули, то tsconfig должны быть как для backend + в подключаемом скрипте в html указать <script src="./build/app2.js" type="module"></script>
-
-
-  Для того что бы файл считался модулем он должен содержать ключевое слово export
-  В tsconfig: "module": "commonjs", "outDir": "./build/"
-
-  ES6
-  в tsconfig: "module": "ES6",
-  в package.json:   "type": "module",
-  в конце импорта указать расширение .js
+  Import и export
 */
 
 export const a = 5;
-export namespace A { // - обертка, позволяющая инкапсулировать в себя некую логику
-  export const a = 5;
 
-  export interface B {
-    c: number;
-  }
+export class Test {}
+
+export const Obj = {}
+export interface B {
+  c: number;
 }
