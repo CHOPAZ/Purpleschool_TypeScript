932af6d4c7422ff2787842464121b49b3c863795
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-06-05 21:40:15 +0500

    Command (Посредник)

--- build/module/app.js
+++ build/module/app.js
@@ -2,73 +2,46 @@
 /*
   Поведенческие паттерны - решают задачу эффективного взаимодействия между компонентами.
 
-  1. Chain of Command / Цепочка вызово - практически в любой API, где есть обработчики. Задача Chain of Command построить такую цепоку,
-    которая бы отрабатывала входящий запрос-например, и передавала бы это друг за другом
-
+  2. Mediator (посредник) - используется на фронтенде для связывания компонентов, которые разрознены и не знают друг о друге
 */
-class AbstractMiddleWare {
-    next(mid) {
-        this.nextMiddleWare = mid;
-        return mid;
+class Mediated {
+    setMediator(mediator) {
+        this.mediator = mediator;
     }
-    handle(request) {
-        if (this.nextMiddleWare) {
-            return this.nextMiddleWare.handle(request);
+}
+class Notifications {
+    send() {
+        console.log('Отправляю уведомление');
+    }
+}
+class Log {
+    log(message) {
+        console.log(message);
+    }
+}
+class EventHandler extends Mediated {
+    myEvent() {
+        this.mediator.notify('EventHamdler', 'myEvent');
+    }
+}
+class NotificationsMediator {
+    constructor(notifications, logger, handler) {
+        this.notifications = notifications;
+        this.logger = logger;
+        this.handler = handler;
+    }
+    notify(_, event) {
+        switch (event) {
+            case 'myEvent':
+                this.notifications.send();
+                this.logger.log('Отправлено');
+                break;
         }
-        return;
     }
 }
-/* Авторизация */
-class AuthMiddleWare extends AbstractMiddleWare {
-    handle(request) {
-        console.log('AuthMiddleWare');
-        if (request.userId === 1) {
-            return super.handle(request); // super.handle(request) вызывает метод handle() из абстрактного класса AbstractMiddleWare
-        }
-        return { error: 'Вы не авторизованы' };
-    }
-}
-/* Валидация */
-class ValidateMiddleWare extends AbstractMiddleWare {
-    handle(request) {
-        console.log('ValidateMiddleWare');
-        if (request.body) {
-            return super.handle(request);
-        }
-        return { error: 'Нет тела данных' };
-    }
-}
-/*  */
-class Controller extends AbstractMiddleWare {
-    handle(request) {
-        console.log('Controller');
-        return { success: request };
-    }
-}
-const controller = new Controller();
-const validate = new ValidateMiddleWare();
-const auth = new AuthMiddleWare();
-/* Цепочка вызово */
-auth.next(validate).next(controller);
-/*
-  AuthMiddleWare
-  { error: 'Вы не авторизованы' }
-*/
-console.log(auth.handle({
-    userId: 3
-}));
-/*
-  AuthMiddleWare
-  ValidateMiddleWare
-  { error: 'Нет тела данных' }
-*/
-console.log(auth.handle({
-    userId: 1
-}));
-/*
-
-*/
-console.log(auth.handle({
-    userId: 1,
-    body: 'I am ok'
-}));
+const handler = new EventHandler();
+const logger = new Log();
+const notifications = new Notifications();
+const mediator = new NotificationsMediator(notifications, logger, handler);
+handler.setMediator(mediator);
+handler.myEvent();
--- src/module/app.ts
+++ src/module/app.ts
@@ -1,97 +1,65 @@
 /*  
   Поведенческие паттерны - решают задачу эффективного взаимодействия между компонентами.
 
-  1. Chain of Command / Цепочка вызово - практически в любой API, где есть обработчики. Задача Chain of Command построить такую цепоку, 
-    которая бы отрабатывала входящий запрос-например, и передавала бы это друг за другом 
-
+  2. Mediator (посредник) - используется на фронтенде для связывания компонентов, которые разрознены и не знают друг о друге
 */
 
-interface IMiddleWare {
-  next(mid: IMiddleWare): IMiddleWare;
-  handle(request: any): any;
+interface Mediator {
+  notify(sender: string, event: string): void;
 }
 
-abstract class AbstractMiddleWare implements IMiddleWare{
-  private nextMiddleWare: IMiddleWare;
-
-  next(mid: IMiddleWare): IMiddleWare {
-    this.nextMiddleWare = mid;
-    return mid
+abstract class Mediated {
+  mediator: Mediator;
+  setMediator(mediator: Mediator) {
+    this.mediator = mediator;
   }
-  handle(request: any) {
-    if(this.nextMiddleWare) {
-      return this.nextMiddleWare.handle(request);
+}
+
+class Notifications {
+  send() {
+    console.log('Отправляю уведомление');
+  }
+}
+
+class Log {
+  log(message: string) {
+    console.log(message);
+  }
+}
+
+class EventHandler extends Mediated{
+  myEvent() {
+    this.mediator.notify('EventHamdler', 'myEvent');
+  }
+}
+
+class NotificationsMediator implements Mediator {
+  constructor(
+    public notifications: Notifications,
+    public logger: Log,
+    public handler: EventHandler
+  ) {}
+  notify(_: string, event: string): void {
+    switch (event) {
+      case 'myEvent':
+        this.notifications.send();
+        this.logger.log('Отправлено');
+        break;
     }
-    return;
   }
   
 }
 
-/* Авторизация */
-class AuthMiddleWare extends AbstractMiddleWare {
-  override handle(request: any) {
-    console.log('AuthMiddleWare');
-    if (request.userId === 1) {
-      return super.handle(request); // super.handle(request) вызывает метод handle() из абстрактного класса AbstractMiddleWare
-    }
-    return { error: 'Вы не авторизованы'}
-  }
-}
+const handler = new EventHandler();
+const logger = new Log();
+const notifications = new Notifications();
 
-/* Валидация */
-class ValidateMiddleWare extends AbstractMiddleWare {
-  override handle(request: any) {
-    console.log('ValidateMiddleWare');
-    if (request.body) {
-      return super.handle(request);
-    }
-    return { error: 'Нет тела данных'}
-  }
-}
+const mediator = new NotificationsMediator(
+  notifications,
+  logger,
+  handler
+);
 
-/*  */
-class Controller extends AbstractMiddleWare {
-  override handle(request: any) {
-    console.log('Controller');
-    
-    return { success: request}
-  }
-}
+handler.setMediator(mediator);
 
-const controller = new Controller();
-const validate = new ValidateMiddleWare();
-const auth = new AuthMiddleWare();
-
-/* Цепочка вызово */
-auth.next(validate).next(controller)
-
-
-/* 
-  AuthMiddleWare
-  { error: 'Вы не авторизованы' }
-*/
-console.log(auth.handle({
-  userId: 3
-}));
-
-
-/* 
-  AuthMiddleWare
-  ValidateMiddleWare
-  { error: 'Нет тела данных' }
-*/
-console.log(auth.handle({
-  userId: 1
-}));
-
-/* 
-  AuthMiddleWare
-  ValidateMiddleWare
-  Controller
-  { success: { userId: 1, body: 'I am ok' } }
-*/
-console.log(auth.handle({
-  userId: 1,
-  body: 'I am ok'
-}));
-
+handler.myEvent()
