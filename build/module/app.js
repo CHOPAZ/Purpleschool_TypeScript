cbca05848a6291dd781b0acad3fcdf1afe5ff523
Pavel Kvasnitsky <paxa627894@yandex.ru>
2023-06-06 20:51:28 +0500

    Iterator (Итератор)

--- build/module/app.js
+++ build/module/app.js
@@ -2,43 +2,71 @@
 /*
   Поведенческие паттерны - решают задачу эффективного взаимодействия между компонентами.
 
-  5. Strategy - поведенческий алгоритм, в рамках которого, выделяем схожее поведение (алгоритмы) в нашей системе в отдельные классы,
-    после чего можно быстро их испольвзовать взаимозаменяя друг другом
+  6. Iterator - итерироваться по коллекции, необходим, когда необходимо пройти одну и туже структуру данных разными способами
+  (например: обойти по приоритету, обойти по зависимостям)
 */
-class User {
-}
-class Auth {
-    constructor(strategy) {
-        this.strategy = strategy;
-    }
-    setStrategy(strategy) {
-        this.strategy = strategy;
-    }
-    authUser(user) {
-        return this.strategy.auth(user);
+class Task {
+    constructor(prioritet) {
+        this.prioritet = prioritet;
     }
 }
-class JWTStrategy {
-    auth(user) {
-        if (user.jwtToken) {
-            //логика
-            return true;
-        }
-        return false;
+class TaskList {
+    constructor() {
+        this.tasks = [];
+    }
+    sortByPriority() {
+        this.tasks = this.tasks.sort((a, b) => {
+            if (a.prioritet > b.prioritet) {
+                return 1;
+            }
+            else if (a.prioritet === b.prioritet) {
+                return 0;
+            }
+            else {
+                return -1;
+            }
+        });
+    }
+    addTask(task) {
+        this.tasks.push(task);
+    }
+    getTask() {
+        return this.tasks;
+    }
+    countTasks() {
+        return this.tasks.length;
+    }
+    getIterator() {
+        return new PriorityTaskIterator(this);
     }
 }
-class GitHubStrategy {
-    auth(user) {
-        if (user.githubToken) {
-            //логика
-            return true;
-        }
-        return false;
+class PriorityTaskIterator {
+    constructor(taskList) {
+        this.idxElem = 0;
+        taskList.sortByPriority();
+        this.taskList = taskList;
+    }
+    current() {
+        return this.taskList.getTask()[this.idxElem];
+    }
+    next() {
+        this.idxElem += 1;
+        return this.taskList.getTask()[this.idxElem];
+    }
+    prev() {
+        this.idxElem -= 1;
+        return this.taskList.getTask()[this.idxElem];
+    }
+    index() {
+        return this.idxElem;
     }
 }
-const user = new User();
-user.jwtToken = 'token';
-const auth = new Auth(new JWTStrategy());
-console.log(auth.authUser(user));
-auth.setStrategy(new GitHubStrategy());
-console.log(auth.authUser(user));
+const taskList = new TaskList();
+taskList.addTask(new Task(8));
+taskList.addTask(new Task(1));
+taskList.addTask(new Task(3));
+const iterator = taskList.getIterator();
+console.log(iterator.current());
+console.log(iterator.next());
+console.log(iterator.prev());
+console.log(iterator.index());
--- src/module/app.ts
+++ src/module/app.ts
@@ -1,56 +1,93 @@
 /*  
   Поведенческие паттерны - решают задачу эффективного взаимодействия между компонентами.
 
-  5. Strategy - поведенческий алгоритм, в рамках которого, выделяем схожее поведение (алгоритмы) в нашей системе в отдельные классы,
-    после чего можно быстро их испольвзовать взаимозаменяя друг другом
+  6. Iterator - итерироваться по коллекции, необходим, когда необходимо пройти одну и туже структуру данных разными способами
+  (например: обойти по приоритету, обойти по зависимостям)
 */
 
-class User {
-  githubToken: string;
-  jwtToken: string;
+class Task {
+  constructor(public prioritet: number) {}
 }
 
-interface AuthStratуgy {
-  auth(user: User): boolean;
-}
+class TaskList {
+  private tasks: Task[] = [];
 
-class Auth {
-  constructor(private strategy: AuthStratуgy) {}
-
-  setStrategy(strategy: AuthStratуgy) {
-    this.strategy = strategy
+  public sortByPriority() {
+    this.tasks =this.tasks.sort((a, b) => {
+      if (a.prioritet > b.prioritet) {
+        return 1
+      } else if (a.prioritet === b.prioritet) {
+        return 0;
+      } else {
+        return -1;
+      }
+    })
   }
-  public authUser(user: User): boolean {
-    return this.strategy.auth(user)
-  }
-}
 
-class JWTStrategy implements AuthStratуgy {
-  auth(user: User): boolean {
-    if (user.jwtToken) {
-      //логика
-      return true
-    }
-    return false
+  public addTask(task: Task) {
+    this.tasks.push(task);
   }
-  
-}
 
-class GitHubStrategy implements AuthStratуgy {
-  auth(user: User): boolean {
-    if (user.githubToken) {
-      //логика
-      return true
-    }
-    return false
+  public getTask() {
+    return this.tasks;
+  }
+
+  public countTasks() {
+    return this.tasks.length;
+  }
+
+  getIterator() {
+    return new PriorityTaskIterator(this)
   }
 }
 
-const user = new User();
-user.jwtToken = 'token';
 
-const auth = new Auth(new JWTStrategy());
-console.log(auth.authUser(user));
 
-auth.setStrategy(new GitHubStrategy());
-console.log(auth.authUser(user));
+interface IIterator<T> {
+  /* текущий элемент */
+  current(): T | undefined;
+  /* следующий элемент */
+  next(): T | undefined;
+  /* предыдущий элемент */
+  prev():  T | undefined;
+  /* текущий индекс элемента */
+  index(): number;
+}
+
+class PriorityTaskIterator implements IIterator<Task>{
+  private idxElem: number = 0;
+  private taskList: TaskList;
+
+  constructor(taskList: TaskList) {
+    taskList.sortByPriority();
+    this.taskList = taskList;
+  }
+
+  current(): Task | undefined {
+    return this.taskList.getTask()[this.idxElem]
+  }
+  next(): Task | undefined {
+    this.idxElem += 1;
+    return this.taskList.getTask()[this.idxElem]
+  }
+  prev(): Task | undefined {
+    this.idxElem -= 1;
+    return this.taskList.getTask()[this.idxElem]
+  }
+  index(): number {
+    return this.idxElem
+  }
+
+}
+
+const taskList = new TaskList();
+taskList.addTask(new Task(8));
+taskList.addTask(new Task(1));
+taskList.addTask(new Task(3));
+
+const iterator = taskList.getIterator();
+console.log(iterator.current());
+console.log(iterator.next());
+console.log(iterator.prev());
+console.log(iterator.index());
+
